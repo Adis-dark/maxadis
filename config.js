2b79c0fa5ecf7544d38190a55a25ca1d37cbb384
Max_Adis <maxhounkpatin001@gmail.com>
2026-04-02 23:48:12 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+/**
+ * Max_Adis - Configuration Globale (Firebase & APIs)
+ */
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD8Fa358PvORR8OfsXVa_vWTdktw9jU-dI",
+  authDomain: "maxadis-app.firebaseapp.com",
+  projectId: "maxadis-app",
+  storageBucket: "maxadis-app.firebasestorage.app",
+  messagingSenderId: "662021256222",
+  appId: "1:662021256222:web:3d37d74642bc92850bea0f"
+};
+
+const IMGBB_KEY = '94856aba90d8fb6881ab8b2716fd8ebf';
+const GROQ_KEY  = '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';
+const CEREBRAS_KEY = 'nwpjxm5ttmhth5myc5mc68fvxkw3r28hc56jtften4vyd93j';
+
+// Numéro WhatsApp pour les commandes
+const WA_NUM = '2290154116425';
+
+// Email de contact
+const CONTACT_EMAIL = 'maxadiscontact@gmail.com';
+
+// Nom du site
+const SITE_NAME = 'Max_Adis';
+
+// Devise
+const CURRENCY = 'FCFA';
